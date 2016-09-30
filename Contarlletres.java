feb1c3aa31bcdcf16ce90e0d8ff0e4d55724e594
Ulli11 <sergicaf11@gmail.com>
2016-09-30 19:32:12 +0200

    Versio1

---
+++ Contarlletres.java
@@ -0,0 +1,19 @@
+
+public class Contarlletres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String frase = "Hola";
+		
+		if (4%2==0) {
+			System.out.println(frase);
+			
+		}
+		else {
+			System.out.println("No està bé");
+		}
+
+	}
+
+}
