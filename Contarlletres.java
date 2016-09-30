417498732182b775dc3d04edff92c4bcfc3b9c01
Ulli11 <sergicaf11@gmail.com>
2016-09-30 19:41:04 +0200

    Versio3

--- Contarlletres.java
+++ Contarlletres.java
@@ -4,6 +4,7 @@ public class Contarlletres {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Hola
+		//Comentari del 7
 		String frase = "Hola";
 		
 		if (4%2==0) {
